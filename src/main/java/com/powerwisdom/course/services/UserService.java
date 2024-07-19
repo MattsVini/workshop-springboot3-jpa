71f3c304248549adb7329d7ae2f82164802d52c3
Mateus Vinicios <mateusvinicios214@gmail.com>
2024-07-19 00:35:19 -0300

    Exception handling - update

--- src/main/java/com/powerwisdom/course/services/UserService.java
+++ src/main/java/com/powerwisdom/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.powerwisdom.course.repositories.UserRepository;
 import com.powerwisdom.course.services.exceptions.DatabaseException;
 import com.powerwisdom.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -36,16 +38,21 @@ public class UserService {
 		try {
 		repository.deleteById(id);
 		} catch(EmptyResultDataAccessException e) {
-			 new ResourceNotFoundException(id);
+			throw new ResourceNotFoundException(id);
 		} catch(DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
 	}
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		} catch(EntityNotFoundException e) {
+			e.printStackTrace();
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
